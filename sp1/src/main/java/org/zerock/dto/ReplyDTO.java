69b6295bfbcd2e1c4da3a442b9868a3447d5055c
AhnChanghyun <ach880203@gmail.com>
2025-12-16 18:28:36 +0900

    React Study

---
+++ sp1/src/main/java/org/zerock/dto/ReplyDTO.java
@@ -0,0 +1,35 @@
+package org.zerock.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*create table tbl_reply(
+		rno int auto_increment primary key,
+	    replyText varchar(500) not null,
+	    replyer varchar(50) not null,
+	    replydate timestamp default now(),
+	    updatedate timestamp default now(),
+	    delflag boolean default false,
+	    bno int not null
+	    );
+*/
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyDTO {
+
+		private int rno;
+		private String replyText;
+		private String replyer;
+		private LocalDateTime replyDate;
+		private LocalDateTime updatedate;
+		private boolean delflag;
+		
+		private Long bno;
+	
+}
---
+++ sp1/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -0,0 +1,57 @@
+package org.zerock.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.dto.ReplyDTO;
+
+import lombok.extern.log4j.Log4j2;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+class ReplyMapperTests {
+
+		@Autowired
+		private ReplyMapper replyMapper;
+		
+		@Test
+		public void tetsInsert() {
+			
+			ReplyDTO replyDTO = ReplyDTO.builder()
+						.bno(49999L)
+						.replyText("댓글 내용")
+						.replyer("홍길동")
+						.build();
+		int result = replyMapper.insert(replyDTO);
+		log.info("result ; " + result);
+		log.info("rno ; " + replyDTO.getRno());
+		}
+		
+		@Test
+		public void testRead() {
+			ReplyDTO dto = replyMapper.read(1);
+			log.info("dto : " + dto);
+		}
+		
+		@Test
+		public void testDelete() {
+			log.info("result : " + replyMapper.delete(1));
+		}
+		
+		@Test
+		public void testUpdate() {
+			ReplyDTO replyDTO = new ReplyDTO();
+			
+			replyDTO.setReplyText("댓글 내용 수정");
+			replyDTO.setRno(1);
+			
+			replyMapper.update(replyDTO);
+		}
+		
+		
+}
