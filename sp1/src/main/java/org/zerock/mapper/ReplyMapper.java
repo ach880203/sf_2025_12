13a565f63b52878f8aabb608950e8179b9cbb3ca
AhnChanghyun <ach880203@gmail.com>
2025-12-16 18:56:56 +0900

    React Study

--- sp1/src/main/java/org/zerock/mapper/ReplyMapper.java
+++ sp1/src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -6,15 +6,16 @@ import org.apache.ibatis.annotations.Param;
 import org.zerock.dto.BoardDTO;
 import org.zerock.dto.ReplyDTO;
 
-
 public interface ReplyMapper {
 
 	int insert(ReplyDTO replyDTO);
-	
+
 	ReplyDTO read(@Param("rno") int rno);
-	
+
 	int delete(@Param("rno") int rno);
-	
+
 	int update(ReplyDTO replyDTO);
-		  
+
+	List<ReplyDTO> listOfBoard(@Param("bno") Long bno, @Param("skip") int skip, @Param("limit") int limit);
+
 }
--- sp1/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ sp1/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -16,42 +16,53 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 class ReplyMapperTests {
 
-		@Autowired
-		private ReplyMapper replyMapper;
-		
-		@Test
-		public void tetsInsert() {
-			
-			ReplyDTO replyDTO = ReplyDTO.builder()
-						.bno(49999L)
-						.replyText("댓글 내용")
-						.replyer("홍길동")
-						.build();
+	@Autowired
+	private ReplyMapper replyMapper;
+
+	@Test
+	public void tetsInsert() {
+
+		ReplyDTO replyDTO = ReplyDTO.builder().bno(49999L).replyText("댓글 내용").replyer("홍길동").build();
 		int result = replyMapper.insert(replyDTO);
 		log.info("result ; " + result);
 		log.info("rno ; " + replyDTO.getRno());
+	}
+
+	@Test
+	public void testRead() {
+		ReplyDTO dto = replyMapper.read(1);
+		log.info("dto : " + dto);
+	}
+
+	@Test
+	public void testDelete() {
+		log.info("result : " + replyMapper.delete(1));
+	}
+
+	@Test
+	public void testUpdate() {
+		ReplyDTO replyDTO = new ReplyDTO();
+
+		replyDTO.setReplyText("댓글 내용 수정");
+		replyDTO.setRno(1);
+
+		replyMapper.update(replyDTO);
+	}
+
+	@Test
+	public void testInserts() {
+		long[] bnos = { 49999L, 49998L, 49997L };
+
+		for (Long bno : bnos) {
+			for (int i = 0; i < 100; i++) {
+				ReplyDTO dto = ReplyDTO.builder().bno(bno).replyText("replyer" + i).replyer("replyer" + i).build();
+				replyMapper.insert(dto);
+			}
 		}
-		
-		@Test
-		public void testRead() {
-			ReplyDTO dto = replyMapper.read(1);
-			log.info("dto : " + dto);
-		}
-		
-		@Test
-		public void testDelete() {
-			log.info("result : " + replyMapper.delete(1));
-		}
-		
-		@Test
-		public void testUpdate() {
-			ReplyDTO replyDTO = new ReplyDTO();
-			
-			replyDTO.setReplyText("댓글 내용 수정");
-			replyDTO.setRno(1);
-			
-			replyMapper.update(replyDTO);
-		}
-		
-		
+	}
+	@Test
+	public void testList() {
+		replyMapper.listOfBoard(49999L, 10, 10)
+		.forEach(reply -> log.info("reply: " +reply));
+	}
 }
